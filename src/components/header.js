b82180969a4e3b4a3f20acb9819c758263777650
Warre333 <warre.bossaert@bazandpoort.be>
2022-05-05 14:29:21 +0200

    Added

--- src/components/header.js
+++ src/components/header.js
@@ -25,10 +25,10 @@ function header() {
                     </a>
 
                     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" class="nav-link px-2 link-secondary">Homepage</a></li>
+                        <li><a href="#" class="nav-link px-2 link-dark">Explore</a></li>
                         <li><a href="#" class="nav-link px-2 link-dark">Friends</a></li>
-                        <li><a href="#" class="nav-link px-2 link-dark">Customers</a></li>
-                        <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>
+                        <li><a href="#" class="nav-link px-2 link-dark">Messages</a></li>
+                        {/* Shopping page */}
                     </ul>
 
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -42,6 +42,8 @@ function header() {
                         <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
                             <li><a class="dropdown-item" href="#">Profile</a></li>
                             <li><a class="dropdown-item" href="#">Create Post</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+
                             <li><hr class="dropdown-divider" /></li>
                             <li><a class="dropdown-item" href="#">Sign out</a></li>
                         </ul>
